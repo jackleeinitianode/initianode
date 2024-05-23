feec3534d2bad3c50d53572e9d7716533575d05d
jackleeinitianode <jackleeinitianode@gmail.com>
2024-05-23 13:27:59 +0800

    Update initia-setup.sh

--- initia-setup.sh
+++ initia-setup.sh
@@ -6,7 +6,7 @@ fi
 
 
 function install_environment() {
-    # CHECKPOINT  install go
+    # 1. System updates, installation of required environments
 	sudo apt-get update
 	sudo apt-get install clang cmake build-essential
 	
@@ -15,17 +15,48 @@ function install_environment() {
 	sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 	export PATH=$PATH:/usr/local/go/bin
 	
-	# 3. install rustup (When the selection for 1, 2, or 3 appears, just press Enter.)
+	# 3. install pm2
+	npm install -g pm2
+	
+	# 4. install rustup (When the selection for 1, 2, or 3 appears, just press Enter.)
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	
-	# 4. git clone
+	# 5. git clone
 	git clone -b v0.2.0 https://github.com/0glabs/0g-storage-node.git
 
-	# 5. build
+	# 6. build
 	cd $HOME/0g-storage-node
 	git submodule update --init
 	sudo apt install cargo
 	cargo build --release
+	
+	# 7. setup variable (at once)
+	echo 'export ZGS_LOG_DIR="$HOME/0g-storage-node/run/log"' >> ~/.bash_profile
+	echo 'export ZGS_LOG_CONFIG_FILE="$HOME/0g-storage-node/run/log_config"' >> ~/.bash_profile
+	echo 'export LOG_CONTRACT_ADDRESS="0x2b8bC93071A6f8740867A7544Ad6653AdEB7D919"' >> ~/.bash_profile
+	echo 'export MINE_CONTRACT="0x228aCfB30B839b269557214216eA4162db24445d"' >> ~/.bash_profile
+	source ~/.bash_profile
+	echo -e "ZGS_LOG_DIR: $ZGS_LOG_DIR\nZGS_LOG_CONFIG_FILE: $ZGS_LOG_CONFIG_FILE\nLOG_CONTRACT_ADDRESS: $LOG_CONTRACT_ADDRESS\nMINE_CONTRACT: $MINE_CONTRACT\n\n\033[33mby Nodebrand.\033[0m"
+	
+	# 8. Extract and Store private_key
+	# type and store your private key
+	read -sp "Enter your private key: " PRIVATE_KEY && echo
+	sed -i 's|miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|' $HOME/0g-storage-node/run/config.toml
+	
+	# 9. Update your config.toml
+	sed -i 's|# log_config_file = "log_config"|log_config_file = "'"$ZGS_LOG_CONFIG_FILE"'"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|# log_directory = "log"|log_directory = "'"$ZGS_LOG_DIR"'"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|mine_contract_address = ".*"|mine_contract_address = "'"$MINE_CONTRACT"'"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|log_contract_address = ".*"|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|blockchain_rpc_endpoint = "https://rpc-testnet.0g.ai"|blockchain_rpc_endpoint = "https://0g-evm.rpc.nodebrand.xyz"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|# network_dir = "network"|network_dir = "network"|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|# network_libp2p_port = 1234|network_libp2p_port = 1234|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmPxGNWu9eVAQPJww79J32pTJLKGcpjRMb4Qb8xxKkyuG1","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAm93Hd5azfhkGBbkx1zero3nYHvfjQYM2NtiW4R3r5bE2g"\]|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|' $HOME/0g-storage-node/run/config.toml
+	sed -i 's|# db_dir = "db"|db_dir = "db"|' $HOME/0g-storage-node/run/config.toml
+	
+	
+	# 10. start pm2
+	pm2 start ../target/release/zgs_node -- --config config.toml
 }
 
 
