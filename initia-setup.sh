71b67fd1b1741953e2f6383f8c6310dd8749fd99
jackleeinitianode <jackleeinitianode@gmail.com>
2024-05-23 13:15:55 +0800

    Update initia-setup.sh

--- initia-setup.sh
+++ initia-setup.sh
@@ -1,5 +1,12 @@
+if [ "$(id -u)" != "0" ]; then
+    echo "root is required."
+    echo "Please use sudo -i switch to root account and rerun the script"
+    exit 1
+fi
+
+
 function install_environment() {
-  # 1. System updates, installation of required environments
+    # CHECKPOINT  install go
 	sudo apt-get update
 	sudo apt-get install clang cmake build-essential
 }
@@ -13,10 +20,10 @@ function install_initia() {
 
 function menu() {
     while true; do
-        echo "######## Twitter: @jleeinitianode ########"
+        echo "########Twitter: @jleeinitianode########"
         echo "1. Install initia Node"
         echo "2. Exit"
-        echo "##########################################"
+        echo "#############################################################"
         read -p "Select function: " choice
         case $choice in
         1)
